a59461183d0d8172b902a9cb6a63185145ae7132
NgHuThanh <148330705+NgHuThanh@users.noreply.github.com>
2023-10-30 16:52:49 +0700

    Add files via upload
    tôi gữi hiển thị tin tức thêm tin tức với menu chung kết nối các trang

---
+++ bainhom/Main/trangchu.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" href="css/trangchu.css"> <!-- Đây là liên kết đến tệp CSS để tùy chỉnh giao diện -->
+</head>
+<body>
+    
+
+    <main>
+        <section class="featured-products">
+            <h2>THẾ GIỚI POKEMON ĐANG CHỜ BẠN</h2>
+            <!-- Danh sách các sản phẩm nổi bật -->
+            <div class="product">
+                <img src="shoe1.jpg" alt="Giày số 1">
+                <h3>Giày thể thao nam</h3>
+                <p>Giá: $99.99</p>
+            </div>
+            <div class="product">
+                <img src="shoe2.jpg" alt="Giày số 2">
+                <h3>Giày cao gót nữ</h3>
+                <p>Giá: $79.99</p>
+            </div>
+        </section>
+
+        <section class="about-us">
+            <h2>Về chúng tôi</h2>
+            <p>Chúng tôi là cửa hàng giày chuyên cung cấp các sản phẩm thời trang và chất lượng.</p>
+        </section>
+    </main>
+
+    <footer>
+        <p>&copy; 2023 Cửa hàng giày</p>
+    </footer>
+</body>
+</html>
